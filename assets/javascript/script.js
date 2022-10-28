0751007868c4391808432da8bae68c2c78bf2deb
BradleyParkin <bradbusiness@hotmail.com>
2022-10-28 16:09:00 +0000

    set up the main structure of the game functions

---
+++ assets/javascript/script.js
@@ -0,0 +1,24 @@
+/** Run Game */
+
+function runGame() {
+
+}
+
+/** Check Answer */
+
+function checkAnswer() {
+
+}
+
+/** Increment Score */
+
+function incrementScore() {
+
+}
+
+/** Increment Computer Score */
+
+function incrementComputerScore() {
+
+}
+
